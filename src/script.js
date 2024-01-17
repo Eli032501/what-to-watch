8ad6f6c84375a746804f312daa9c7c909ba8ced9
Eliana Pereira <98112983+Eli032501@users.noreply.github.com>
2024-01-17 13:42:49 +0000

    JS form output and typewriter

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
-alert("znz");
+function getOutput(event) {
+  event.preventDefault();
+
+  new Typewriter("#output-div", {
+    strings: "Me before You",
+    autoStart: true,
+    delay: 30,
+    cursor: null,
+  });
+}
+
+let formSearch = document.querySelector("#form-search");
+formSearch.addEventListener("click", getOutput);
