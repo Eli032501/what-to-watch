d0ac644d362cb485f2fd182b9350a9744fb779eb
Eliana Pereira <98112983+Eli032501@users.noreply.github.com>
2024-01-20 17:02:30 +0000

    functions of button toggle js -series or movies

--- src/script.js
+++ src/script.js
@@ -12,26 +12,40 @@ function displayResults(response) {
 
 function getOutput(event) {
   event.preventDefault();
+  console.log(inputButton.value);
 
-  let buttonOption = "movies";
+  let buttonOption = inputButton.value;
   let userInput = document.querySelector("#input-user");
   let apiKey = "ba3t92f6af6b8204143cbo1c5a032ba3";
   let prompt = `give-me 3 options of ${buttonOption} in the category of ${userInput.value}`;
+  console.log(prompt);
   let context =
-    "you're well informed about all movis and series, and you know the best to recommend. You never explain your reasons or introduce your answers. You only give the name of the movies or series and the year which it was realeased. You give all your answers in formated in HTML using a <ul> element.";
+    "you're well informed about all movis and series, and you know the best to recommend. You never explain your reasons or introduce your answers. You only give the name of the production and the year which it was realeased. You give all your answers in formated in HTML using a <ul> element.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("generating recomendations");
   let outputDiv = document.querySelector("#output-div");
   outputDiv.classList.remove("disabled");
   new Typewriter("#output-div", {
-    strings: `generating recomendations`,
+    strings: `Generating recomendations...`,
     autoStart: true,
     delay: 30,
     cursor: null,
   });
   axios.get(apiUrl).then(displayResults);
 }
+
+function changeOptionButon() {
+  inputButton.classList.toggle("change-option");
+  if (inputButton.getAttribute("class") === "change-option") {
+    inputButton.value = "TV Series";
+  } else {
+    inputButton.value = "Movies";
+  }
+}
+
+let inputButton = document.querySelector("#button");
+inputButton.addEventListener("click", changeOptionButon);
 
 let formSearch = document.querySelector("#form-search");
 formSearch.addEventListener("click", getOutput);
