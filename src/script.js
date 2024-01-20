ccc56115efa415c0a1f29d5649621b56cfde39f6
Eliana Pereira <98112983+Eli032501@users.noreply.github.com>
2024-01-20 15:22:29 +0000

    added ai insructions and axios

--- src/script.js
+++ src/script.js
@@ -1,12 +1,36 @@
-function getOutput(event) {
-  event.preventDefault();
-
+function displayResults(response) {
+  console.log("recomendations generated ");
   new Typewriter("#output-div", {
-    strings: "Me before You",
+    strings: `<h3>My recommendations for you:</h3>` + response.data.answer,
     autoStart: true,
     delay: 30,
     cursor: null,
   });
+  let typewriterCursor = document.querySelector(".Typewriter__cursor");
+  typewriterCursor.style.marginLeft = "0px";
+}
+
+function getOutput(event) {
+  event.preventDefault();
+
+  let buttonOption = "movies";
+  let userInput = document.querySelector("#input-user");
+  let apiKey = "ba3t92f6af6b8204143cbo1c5a032ba3";
+  let prompt = `give-me 3 options of ${buttonOption} in the category of ${userInput.value}`;
+  let context =
+    "you're well informed about all movis and series, and you know the best to recommend. You never explain your reasons or introduce your answers. You only give the name of the movies or series and the year which it was realeased. You give all your answers in formated in HTML using a <ul> element.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("generating recomendations");
+  let outputDiv = document.querySelector("#output-div");
+  outputDiv.classList.remove("disabled");
+  new Typewriter("#output-div", {
+    strings: `generating recomendations`,
+    autoStart: true,
+    delay: 30,
+    cursor: null,
+  });
+  axios.get(apiUrl).then(displayResults);
 }
 
 let formSearch = document.querySelector("#form-search");
